509ffb1cd9d221eaaf190020c5acbae143381ef1
Isaac Watson <iawatson@oakland.edu>
2018-09-28 16:02:56 -0400

    Added placeholder player with animations

---
+++ Project Realm/Assets/Scripts/Entity.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Entity: any object that can move around
+ * abstract means the class is not able to be instantiated.
+ * 
+ */
+public abstract class Entity : MonoBehaviour {
+    //A 2D Vector that holds the direction the entity is moving (up, down, left, right)
+    protected Vector2 direction;
+
+    //Enumerator to determine entity state
+    protected enum PlayerState
+    {
+        Idle, Walking, Attacking, Dying
+    }
+
+    protected PlayerState animState;
+
+    [SerializeField] //Lets a private or protected variable be modified in the editor
+    protected int entitySpeed;
+
+    protected Animator animator;
+
+	// Use this for initialization
+	protected virtual void Start () {
+        animator = GetComponent<Animator>();
+        animState = PlayerState.Idle;
+	}
+	
+	/* Update is called once per frame.
+     * A virtual method is an abstract method that also provides its own implementation.
+     */
+	protected virtual void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        /*transform gets the transform component of THIS gameObject.
+          Translate is a function to move THIS gameObject towards a direction relative
+           to itself.*/
+        transform.Translate(direction.normalized * Time.deltaTime * entitySpeed);
+        AnimateEntity(direction);
+    }
+    private void AnimateEntity(Vector2 direction)
+    {
+        if(direction.x > 0 || direction.x < 0 || direction.y > 0 || direction.y < 0)
+        {
+            animator.SetLayerWeight(1, 1);
+        } else if(direction == Vector2.zero)
+        {
+            animator.SetLayerWeight(1, 0);
+            animator.SetLayerWeight(0, 1);
+        }
+    }
+
+    
+
+}
---
+++ Project Realm/Assets/Scripts/Player.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity {
+
+    private Vector2 oldDirection;
+
+    //Vector to store the position of the enitity in the previous frame
+    protected Vector2 oldPosition;
+
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
+        entitySpeed = 8; //Should be changed to the player's class's default speed
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        base.Update(); //Calls the parent class's version of update
+        GetInput(); //Check for input every frame
+	}
+
+    private void GetInput()
+    {
+        oldPosition = transform.position;
+        oldPosition = oldPosition.normalized; 
+
+        /*Since Move is called every frame, we want to make sure the player doesn't move
+        when we let go of our input.*/
+        direction = Vector2.zero;
+        //
+        animState = PlayerState.Idle;
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            direction += Vector2.up;
+            oldDirection = Vector2.up;
+            animState = PlayerState.Walking;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction += Vector2.down;
+            oldDirection = Vector2.down;
+            animState = PlayerState.Walking;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction += Vector2.left;
+            oldDirection = Vector2.left;
+            animState = PlayerState.Walking;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += Vector2.right;
+            oldDirection = Vector2.right;
+            animState = PlayerState.Walking;
+        }
+
+       
+
+
+        /* x > 0 -> right     x & y > 0 -> right
+         * don't face left or right if x = 0
+         * 
+         */ 
+        if(animState == PlayerState.Walking)
+        {
+            if(direction.x == 0)
+            {
+                if(direction.y > 0)
+                {
+                    animator.Play("astronaut_walk_up_16x16");
+                } else
+                {
+                    animator.Play("astronaut_walk_down_16x16");
+                }
+            } else if(direction.x > 0)
+            {
+                animator.Play("astronaut_walk_right_16x16");
+            } else
+            {
+                animator.Play("astronaut_walk_left_16x16");
+            }
+        } else if(animState == PlayerState.Idle)
+        {
+            if (oldDirection.x == 0)
+            {
+                if (oldDirection.y > 0)
+                {
+                    animator.Play("astronaut_ide_up_16x16");
+                }
+                else
+                {
+                    animator.Play("astronaut_ide_down_16x16");
+                }
+            }
+            else if (oldDirection.x > 0)
+            {
+                animator.Play("astronaut_ide_right_16x16");
+            }
+            else
+            {
+                animator.Play("astronaut_ide_left_16x16");
+            }
+        }
+       if(Input.GetKey(KeyCode.Alpha1))
+        {
+            transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * 100);
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            transform.Rotate(new Vector3(0, 0, -1) * Time.deltaTime * 100);
+        } 
+    }
+}
